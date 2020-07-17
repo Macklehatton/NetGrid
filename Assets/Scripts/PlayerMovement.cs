18318b7a67e7e5cdc27fce896299332cca9e40b8
macklehatton <macklehatton@gmail.com>
2020-07-17 14:19:56 -0800

    Basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Mirror;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float holdDelay;
+
+    [SerializeField]
+    private Vector3 movementVector;
+    [SerializeField]
+    private float movementScale;
+
+    private float timeSincePress;
+
+    private void Update()
+    {
+        timeSincePress += Time.deltaTime;
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        movementVector = new Vector3(moveX, 0.0f, moveY);
+
+        if (timeSincePress > holdDelay)
+        {
+            if (movementVector != Vector3.zero)
+            {
+                transform.position += movementVector * movementScale;
+                timeSincePress = 0.0f;
+            }
+        }
+    }
+}
