6cf4b62c02cc4fd9ca4a5744d9d15b2a4cd84b0b
TheNotary <no@email.plz>
2020-07-19 14:59:15 -0500

    my editor set us using 1.1.3 of test-framework?

---
+++ Assets/Scripts/Wasty.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [InitializeOnLoad]
+public class Wasty
+{
+    static Wasty()
+    {
+        // EditorApplication.update += Update;
+        Debug.Log("Initializing");
+    }
+
+    static void Update()
+    {
+        Debug.Log("Updating");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
