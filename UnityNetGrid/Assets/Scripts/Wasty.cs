bae8977fbf214b0fa06a4658dedb0ebbaa9f8ba7
TheNotary <wastyx@gmail.com>
2020-07-19 19:55:26 -0500

    Subfolders unity client (#4)
    * my editor set us using 1.1.3 of test-framework?
    
    * moves unity client into it's own subfolder
    
    * another rename
    
    * infinity on movement
    
    * fixing up cs file
    
    * ties in Wasty script to run at startup
    
    * dialing down dependencies
    
    * more dependency dialdowns
    
    * deletes cruft

---
+++ UnityNetGrid/Assets/Scripts/Wasty.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder (-1000)]
+public class Wasty : MonoBehaviour
+{
+    private void MyMethod()
+    {
+        // EditorApplication.update += Update;
+        Debug.Log("Initializing");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyMethod();
+    }
+}
