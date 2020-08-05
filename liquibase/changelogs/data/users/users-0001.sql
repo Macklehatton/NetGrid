8969cd3bdae5021ee9dbc61953fd0d8e20571e82
TheNotary <no@email.plz>
2020-08-05 12:44:23 -0500

    working on seed data

---
+++ liquibase/changelogs/data/users/users-0001.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO users(id, name, bio, is_kind, created_at, updated_at)
+     VALUES (
+              1,               -- id
+              'Mario',         -- name
+              'Great jumper',  -- bio
+              1,               -- is_kind
+              NOW(),           -- created_at
+              NOW()            -- updated_at
+            );
+
+
+INSERT INTO users(id, name, bio, is_kind, created_at, updated_at)
+     VALUES (
+              2,               -- id
+              'Luigi',         -- name
+              'Another good jumper',  -- bio
+              1,               -- is_kind
+              NOW(),           -- created_at
+              NOW()            -- updated_at
+            );
+
+
+INSERT INTO users(id, name, bio, is_kind, created_at, updated_at)
+     VALUES (
+              3,               -- id
+              'Bowser',        -- name
+              'Not such a jumper here',  -- bio
+              0,               -- is_kind
+              NOW(),           -- created_at
+              NOW()            -- updated_at
+            );
