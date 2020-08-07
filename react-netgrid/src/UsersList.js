af823b9f4eae6d5188919414f6b4f55144d1e1f9
TheNotary <no@email.plz>
2020-08-06 20:34:20 -0500

    figure out how to connect the react frontend to the java backend

--- react-netgrid/src/UsersList.js
+++ react-netgrid/src/UsersList.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-const allUsers = [
-  {id: 1, name: "Mario", bio: "Great jumper", is_kind: true, created_at: "2012-01-01T00:31:50Z", updated_at: "2012-01-01T00:31:50Z"},
-  {id: 2, name: "Luigi", bio: "Another good jumper", is_kind: true, created_at: "2012-01-01T00:31:50Z", updated_at: "2012-01-01T00:31:50Z"},
-  {id: 3, name: "Bowser", bio: "Not such a jumper here", is_kind: true, created_at: "2012-01-01T00:31:50Z", updated_at: "2012-01-01T00:31:50Z"},
-];
 
 
 function User(props) {
@@ -18,16 +13,56 @@ function User(props) {
   );
 }
 
-function UsersList() {
-  return (
-    <>
-      <h3>Users (x{allUsers.length})</h3>
-      <a href="#">Add New</a>
-      <ul>
-        {allUsers.map( user => <User {...user} /> )}
-      </ul>
-    </>
-  );
+
+class UsersList extends React.Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    users: []
+  };
+
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_API_URL}/users`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            users: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, users } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          <h3>Users (x{users.length})</h3>
+          <a href="#">Add New</a>
+          <ul>
+            {users.map( user => <User {...user} /> )}
+          </ul>
+        </>
+      );
+    }
+  }
+
 }
 
 export default UsersList;
