7f8ab8241f6c122352e11ed977c0152cce394ce0
TheNotary <no@email.plz>
2020-09-15 10:54:30 -0500

    stray test changes

--- react-netgrid/src/App.test.js
+++ react-netgrid/src/App.test.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import store from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/the home page/i)).toBeInTheDocument();
 });
