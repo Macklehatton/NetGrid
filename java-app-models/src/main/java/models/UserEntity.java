96a94e796f7188040c85e41189cdb33dc5432adc
TheNotary <no@email.plz>
2020-10-26 17:27:51 -0500

    fixes up bugs in model now that db is all set

--- java-app-models/src/main/java/models/UserEntity.java
+++ java-app-models/src/main/java/models/UserEntity.java
@@ -32,7 +32,7 @@ public class UserEntity implements Serializable {
     private String password;
 
     @ManyToMany(cascade = CascadeType.MERGE)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
     @Column(name = "active")
