19f44d6cd07eb04fc15aa12a62dc014c0a325ac1
TheNotary <no@email.plz>
2020-10-28 13:56:54 -0500

    fixes some issues with liquibase seed data

--- liquibase/changelogs/data/users/users-0001.sql
+++ liquibase/changelogs/data/users/users-0001.sql
@@ -1,5 +1,9 @@
 --liquibase formatted sql
+--changeset author:me runAlways:true
+--preconditions onFail:HALT onError:HALT
+--validCheckSum any
 
+DELETE FROM users WHERE id = 1;
 INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_at)
      VALUES (
               1,               -- id
@@ -7,12 +11,13 @@ INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_a
               'Great jumper',  -- bio
               1,               -- is_kind
               'a@b.com',       -- email
-              '123456',        -- password
+              '$2a$10$kRR3XA1b9i8KnU.0URldleMVdB1g/vcjiSeBpKJMY5fONw.roTLBi',        -- password 123456
               NOW(),           -- created_at
               NOW()            -- updated_at
             );
 
 
+DELETE FROM users WHERE id = 2;
 INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_at)
      VALUES (
               2,               -- id
@@ -20,12 +25,12 @@ INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_a
               'Another good jumper',  -- bio
               1,               -- is_kind
               'a@b.com',       -- email
-              '123456',        -- password
+              '$2a$10$kRR3XA1b9i8KnU.0URldleMVdB1g/vcjiSeBpKJMY5fONw.roTLBi',        -- password
               NOW(),           -- created_at
               NOW()            -- updated_at
             );
 
-
+DELETE FROM users WHERE id = 3;
 INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_at)
      VALUES (
               3,               -- id
@@ -33,7 +38,7 @@ INSERT INTO users(id, name, bio, is_kind, email, password, created_at, updated_a
               'Not such a jumper here',  -- bio
               0,               -- is_kind
               'a@b.com',       -- email
-              '123456',        -- password
+              '$2a$10$kRR3XA1b9i8KnU.0URldleMVdB1g/vcjiSeBpKJMY5fONw.roTLBi',        -- password
               NOW(),           -- created_at
               NOW()            -- updated_at
             );
