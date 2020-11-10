6aac4b8e2eb8d74dd2cebf5092f936f7f5ee9556
TheNotary <no@email.plz>
2020-11-09 18:59:27 -0600

    I can generate authentication tokens for mario now, but I can't use them to access admin...

--- java-api/src/main/java/configuration/WebSecurityConfiguration.java
+++ java-api/src/main/java/configuration/WebSecurityConfiguration.java
@@ -4,12 +4,12 @@ import org.njax.trinetco.netgrid.java.api.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -50,11 +50,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers(loginPage).permitAll()
                 .antMatchers("/registration").permitAll()
+                .antMatchers("/api/services/controller/user/login").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/users/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and().csrf().disable()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .formLogin()
                 .loginPage(loginPage)
                 .loginPage("/")
